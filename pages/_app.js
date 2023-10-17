43016bd9eaadfa5a89c71a8566b56ef01e508d9a
Benjamin <tankerkzheng1@gmail.com>
2023-10-17 21:51:57 +0800

    added dark theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
