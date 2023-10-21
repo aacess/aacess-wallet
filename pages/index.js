4a734603fecafb222832710a7eef134e1ccce47e
Benjamin <tankerkzheng1@gmail.com>
2023-10-21 13:36:39 +0800

    update index layout

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    // <main
-    //   className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    // >
-    <main>
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
       <h1 className="text-4xl font-bold text-center">
         Welcome to AACESS Wallet
       </h1>
