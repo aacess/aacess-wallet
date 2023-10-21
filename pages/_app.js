5795dbb480d66bb6d32f3685150fcb05317f5854
Benjamin <tankerkzheng1@gmail.com>
2023-10-21 13:08:00 +0800

    added framer motion

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,33 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import RootLayout from "@/components/Layout";
+import { motion } from "framer-motion";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, router }) {
   return (
-    <RootLayout>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </RootLayout>
+    <motion.div
+      key={router.route}
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+        },
+        pageAnimate: {
+          opacity: 1,
+        },
+      }}
+    >
+      <RootLayout>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RootLayout>
+    </motion.div>
   );
 }
