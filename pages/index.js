749305625896bbfdbae37bec6f9b1df550c82f5c
Cheong Yee Chian <cheongyeechian@gmail.com>
2023-10-22 10:14:57 +0800

    import pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
+"use client";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -10,10 +11,20 @@ import {
   DialogTitle,
   DialogTrigger,
   DialogClose,
-} from "@/components/ui/dialog";
+} from "../components/ui/dialog";
+import { useWalletAuth } from "../pages/modules/wallet/hooks/useWalletAuth";
+import ConnectWallet from "../components/ConnectWallet";
+import { Transaction } from "../components/Transaction";
+import { useWindowSize } from "../lib/ui/hooks/useWindowSize";
+import React, { useState } from "react";
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { isConnecting, isConnected, connect, connectionError, wallet } = useWalletAuth();
+  const { width: windowWidth, height: windowHeight } = useWindowSize();
+  const [transactionSuccess, setTransactionSuccess] = useState(false);
   return (
     <main>
       <div className="divSize">
@@ -46,6 +57,33 @@ export default function Home() {
                 </div>
                 <div className="grid grid-cols-4 items-center gap-4">
                   <Button className="w-64">Cometh Wallet</Button>
+                  
+                    {transactionSuccess && (
+                      <Confetti width={windowWidth} height={windowHeight} />
+                    )}
+                  
+                  <div className="md:min-h-[70vh] gap-2 flex flex-col justify-center items-center">
+                    <div className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+                      <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+                        <div className="grid divide-gray-900/5 bg-gray-50">
+                          <ConnectWallet
+                            isConnected={isConnected}
+                            isConnecting={isConnecting}
+                            connect={connect}
+                            connectionError={connectionError}
+                            wallet={wallet}
+                          />
+                        </div>
+
+                        {isConnected && (
+                          <Transaction
+                            transactionSuccess={transactionSuccess}
+                            setTransactionSuccess={setTransactionSuccess}
+                          />
+                        )}
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
               <DialogFooter className="sm:justify-start">
